f089dfc1d4e8f1f2d3f9159b4c3043afd2923b95
Devan Agrawal <devagr@slac.stanford.edu>
2025-05-22 09:53:33 -0700

    ENH: refactor script to use tag-based organization

--- superscore/tests/db/convert_to_collections.py
+++ superscore/tests/db/convert_to_collections.py
@@ -3,7 +3,7 @@ from enum import IntEnum
 
 from superscore.backends.directory import DirectoryBackend
 from superscore.client import Client
-from superscore.model import Collection, Parameter
+from superscore.model import Parameter
 
 DESTINATION = '.prod_pvs/'
 
@@ -35,30 +35,55 @@ if __name__ == "__main__":
         3: ["Subsystem", "", {}],
     }
 
-    global_coll = Collection(description="Placeholder Collection before data model refactor")
+    client = Client(
+        backend=DirectoryBackend(DESTINATION)
+    )
+    client.backend.reset()
 
     for line in lines[1:-1]:
+        tags = {
+            TagGroup.PROGRAM: set(),
+            TagGroup.REGION: set(),
+            TagGroup.AREA: set(),
+            TagGroup.SUBSYSTEM: set(),
+        }
         region, setpoint, readback, alias, area, subsystem, program_id = line.split(",")
-        program = Program(program_id).name
+        program = Program(int(program_id)).name
         program_tags = tag_groups[TagGroup.PROGRAM][2]
         if program not in program_tags.values():
-            program_tag_id = max(program_tags.key()) + 1
-            program_tags.update(program_tag_id=program)
+            try:
+                program_tag_id = max(program_tags.keys()) + 1
+            except ValueError:
+                program_tag_id = 0
+            program_tags[program_tag_id] = program
+            tags[TagGroup.PROGRAM].add(program_tag_id)
 
         region_tags = tag_groups[TagGroup.REGION][2]
         if region not in region_tags.values():
-            region_tag_id = max(region_tags.key()) + 1
-            region_tags.update(region_tag_id=region)
+            try:
+                region_tag_id = max(region_tags.keys()) + 1
+            except ValueError:
+                region_tag_id = 0
+            region_tags[region_tag_id] = region
+            tags[TagGroup.REGION].add(region_tag_id)
 
         area_tags = tag_groups[TagGroup.AREA][2]
         if area not in area_tags.values():
-            area_tag_id = max(area_tags.key()) + 1
-            area_tags.update(area_tag_id=area)
+            try:
+                area_tag_id = max(area_tags.keys()) + 1
+            except ValueError:
+                area_tag_id = 0
+            area_tags[area_tag_id] = area
+            tags[TagGroup.AREA].add(area_tag_id)
 
         subsystem_tags = tag_groups[TagGroup.SUBSYSTEM][2]
         if subsystem not in subsystem_tags.values():
-            subsystem_tag_id = max(subsystem_tags.key()) + 1
-            subsystem_tags.update(subsystem_tag_id=subsystem)
+            try:
+                subsystem_tag_id = max(subsystem_tags.keys()) + 1
+            except ValueError:
+                subsystem_tag_id = 0
+            subsystem_tags[subsystem_tag_id] = subsystem
+            tags[TagGroup.SUBSYSTEM].add(subsystem_tag_id)
 
         readback_param = None
         if readback != "NA":
@@ -68,22 +93,12 @@ if __name__ == "__main__":
                 read_only=True,
             )
         if setpoint == "NA":
-            global_coll.children.append(readback_param)
+            client.backend.save_entry(readback_param, top_level=False)
         else:
             setpoint_param = Parameter(
                 pv_name=setpoint,
                 readback=readback_param,
                 description=alias,
-                tags={
-                    TagGroup.PROGRAM: {program_tag_id},
-                    TagGroup.REGION: {region_tag_id},
-                    TagGroup.AREA: {area_tag_id},
-                    TagGroup.SUBSYSTEM: {subsystem_tag_id},
-                }
+                tags=tags
             )
-            global_coll.children.append(setpoint_param)
-
-    client = Client(
-        backend=DirectoryBackend(DESTINATION)
-    )
-    client.save(global_coll)
+            client.backend.save_entry(setpoint_param, top_level=False)
